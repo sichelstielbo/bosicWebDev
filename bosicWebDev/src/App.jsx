411c6acb73a188c56bbc5ea97716abf9efb63aa6
sichelstielbo <129999134+sichelstielbo@users.noreply.github.com>
2024-05-06 21:55:13 -0400

    5/6 changes

--- bosicWebDev/src/App.jsx
+++ bosicWebDev/src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import About from './Components/About/About'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
 import Hero from './Components/Hero/Hero'
+import MyWork from './Components/MyWork/MyWork'
 import Navbar from './Components/Navbar/Navbar'
+import Services from './Components/Services/Services'
 
 const App = () => {
   return (
@@ -9,6 +13,10 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Services />
+      <MyWork />
+      <Contact />
+      <Footer />
     </div>
   )
 }
--- bosicWebDev/src/Components/Hero/Hero.jsx
+++ bosicWebDev/src/Components/Hero/Hero.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile_img.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 
 const Hero = () => {
     return (
-        <div className='hero'>
+        <div id='home' className='hero'>
             <img src={profile_img} alt="" />
             <h1><span>I'm Bo Sichelstiel,</span> a fullstack developer based out of Georgia</h1>
             <p>I am a fullstack developer with 5 years experience. I graduated from the University of Georgia.</p>
             <div className="hero-action">
-                <div className="hero-connect">Connect with me</div>
+                <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
                 <div className="hero-resume">My resume</div>
             </div>
         </div>
